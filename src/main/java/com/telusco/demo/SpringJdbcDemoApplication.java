54f69ddcc73753520dc07ca2910d02d5f141f605
krishna <krishnakn140@gmail.com>
2025-12-26 18:40:42 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/telusco/demo/SpringJdbcDemoApplication.java
@@ -0,0 +1,14 @@
+package com.telusco.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringJdbcDemoApplication.class, args);
+	}
+
+}
